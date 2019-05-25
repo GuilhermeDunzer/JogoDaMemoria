fb03fb0ab1cfe3e6b5ae4d3a9f02d961cd3ca219
Guilherme <guilhermefunzer@gmail.com>
2019-05-25 11:19:39 -0300

    comportamento de click

--- jogo-da-memoria/game.js
+++ jogo-da-memoria/game.js
@@ -1,4 +1,28 @@
+var a0_0 = document.querySelector('#a0_0');
+var a1_0 = document.querySelector('#a1_0');
+var a0_1 = document.querySelector('#a0_1');
+var a1_1 = document.querySelector('#a1_1');
 
+a0_0.onclick = a0_0clicked;
+a1_0.onclick = a1_0clicked;
+a0_1.onclick = a0_1clicked;
+a1_1.onclick = a1_1clicked;
+
+function a0_0clicked(){
+    console.log("a0_0 click");
+}
+
+function a1_0clicked(){
+    console.log("a1_0 click");
+}
+
+function a0_1clicked(){
+    console.log("a0_1 click");
+}
+
+function a1_1clicked(){
+    console.log("a1_1 click");
+}
 
 
 
